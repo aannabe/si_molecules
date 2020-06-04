7ebff9840bfc5db7d09ac73997d65cc5af1ca455
Abdulgani Annaberdiyev <aannabe@ncsu.edu>
2020-06-04 16:33:34 -0700

    adding various nodes

---
+++ SiH2/qmc/different_nodes_trial/gs_lda/scf.py
@@ -0,0 +1,55 @@
+import sys, os
+from pyscf import gto, scf, cc, dft
+from pyscf.cc import uccsd_t
+import numpy as np
+import pandas as pd
+
+
+workdir = os.path.dirname(os.path.realpath(__file__))
+#basfile = os.path.join(workdir,'si_cody.basis')
+si_basfile = os.path.join(workdir,'Si.aug-cc-pV5Z.basis')
+h_basfile = os.path.join(workdir,'H.cc-pV5Z.basis')
+
+mol = gto.Mole()
+mol.atom = '''
+Si;
+H,  1,  1.51402;
+H,  1,  1.51402,  2,   91.9830;
+'''
+
+mol.basis = {'Si': gto.load(si_basfile,'Si'), 'H': gto.load(h_basfile,'H')}
+mol.spin = 0
+mol.charge = 0
+mol.ecp = {'Si': gto.basis.parse_ecp('''
+Si nelec 10
+Si ul
+1 5.168316 4.000000
+3 8.861690 20.673264
+2 3.933474 -14.818174
+Si S
+2 9.447023 14.832760
+2 2.553812 26.349664
+Si P
+2 3.660001 7.621400
+2 1.903653 10.331583
+'''),
+'H': gto.basis.parse_ecp('''
+H nelec 0
+H ul
+1 21.24359508259891 1.00000000000000
+3 21.24359508259891 21.24359508259891
+2 21.77696655044365 -10.85192405303825
+H S
+2 1.000000000000000 0.00000000000000
+''')}
+
+
+mol.build()
+#m = scf.RHF(mol)
+m = dft.RKS(mol)
+m.xc = 'LDA'
+m.max_cycle = 150
+m.verbose = 4
+m.chkfile='scf.chkfile'
+print ('E(HF)= %g' % m.kernel())
+
